da14656fe4c7e84ff7e26ea331587c541ce86977
Cory Wheeler <corywheeler@gmail.com>
2019-03-31 00:36:44 -0400

    implementation of quicksort
    ended up using these as a resources for understanding
    
    https://en.wikipedia.org/wiki/Quicksort
    http://blog.benoitvallon.com/sorting-algorithms-in-javascript/the-quicksort-algorithm/
    
    for a while my termination control in my partition was off by one, I wasn't
    scanning the entire array. I was going up to endPosition -1 in the
    partition function in my for loop. once that was corrected, partition
    worked as expected.

--- src/quicksort.js
+++ src/quicksort.js
@@ -1,3 +1,40 @@
-module.exports = function (array) {
-	return array;
+module.exports.sort = function (array) {
+	quicksort(array, 0, array.length - 1);
+}
+
+function quicksort(array, startPosition, endPosition) {
+
+	if(startPosition < endPosition) {
+		var partitionIndex = partition(array, startPosition, endPosition);
+
+	    quicksort(array, startPosition, partitionIndex -1)
+		quicksort(array, partitionIndex + 1, endPosition);
+	}
+}
+
+module.exports.partition = partition;
+
+function partition(array, startPosition, endPosition) {
+
+		let pivot = array[endPosition];
+		var pivotIndex = startPosition;
+
+		for(var scanPosition = startPosition; scanPosition < endPosition; scanPosition++) {
+
+			if(array[scanPosition] < pivot) {
+				swap(array, pivotIndex, scanPosition)
+				pivotIndex = pivotIndex + 1;
+			}
+		}
+
+		swap(array, pivotIndex, endPosition);
+		return pivotIndex;
+}
+
+module.exports.swap = swap;
+
+function swap(array, firstPosition, secondPosition) {
+	var firstPositionValue = array[firstPosition];
+	array[firstPosition] = array[secondPosition];
+	array[secondPosition] = firstPositionValue;
 }
--- test/quicksortSpec.js
+++ test/quicksortSpec.js
@@ -1,15 +1,39 @@
 const chai = require('chai');
 chai.should();
-const quickSort = require('../src/quicksort');
+const quicksort = require('../src/quicksort');
 
 describe('quicksort', () => {
 
-	let unsorted = [1,5,9,10,2];
-	const sorted = [1,2,5,9,10];
+	it.only('should sort the array', () => {
+		var subjectArray = [30,12,4,10,9,3,22,15];
+		var expected = [3,4,9,10,12,15,22,30];
 
-	it('should run', () => {
-		let quicksorted = quickSort(unsorted);
+		quicksort.sort(subjectArray);
+		subjectArray.should.deep.equal(expected);
+	});
 
-		quicksorted.should.equal(sorted)
-	})
+	it.only('should be able to swap elements', () => {
+		var subjectArray = [9,2];
+		var swapped = [2,9];
+
+		quicksort.swap(subjectArray,0,1);
+		subjectArray.should.deep.equal(swapped);
+	});
+
+	it.only('should partition arrays', () => {
+
+		var subjectArray = [12,4,10,9,3, 7];
+		var paritionValue = subjectArray[subjectArray.length - 1];
+
+		var valuesLessThanPartition = [4,3];
+		var valuesGreaterThanPartition = [9, 12, 10];
+
+		var partitionIndex = quicksort.partition(subjectArray, 0, 5);
+
+		subjectArray.should.deep.equal([4, 3, 7, 9, 12, 10]);
+		partitionIndex.should.equal(2);
+		subjectArray[partitionIndex].should.equal(paritionValue);
+		subjectArray.slice(0, partitionIndex).should.deep.equal(valuesLessThanPartition);
+		subjectArray.slice(partitionIndex + 1, subjectArray.length).should.deep.equal(valuesGreaterThanPartition);
+	});
 })
